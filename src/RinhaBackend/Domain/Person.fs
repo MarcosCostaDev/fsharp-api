f072edcaae67ffed1954ad92369d47dc29630406
Marcos Costa <marcos.lcosta@live.com>
2023-08-29 21:53:38 -0400

    Initial Commit

---
+++ src/RinhaBackend/Domain/Person.fs
@@ -0,0 +1,45 @@
+﻿module Domain
+
+open System
+
+
+type Person = { Id:Guid; Name:string; Nickname:string; BirthDay:DateTime; Stack:String[] }
+
+let validPerson person =
+  let validName p = not (String.IsNullOrWhiteSpace p.Name) && p.Name.Length <= 100
+  let validNickName p = not (String.IsNullOrWhiteSpace p.Nickname) && p.Nickname.Length <= 32
+  let validStack p =  match p.Stack with 
+                      | null -> true
+                      | item -> Array.forall (fun element -> not (String.IsNullOrWhiteSpace element)) item
+  let valid p = validName p && validNickName p && validStack p
+  valid person
+
+
+let ValidPerson = {Id = Guid.NewGuid(); 
+                  Name = "Nome teste"; 
+                  Nickname = "apelido teste"; 
+                  BirthDay = DateTime.Now;
+                  Stack = [| "F#"; "Elixir"; "Clojure" |] }
+
+let InvalidPerson = {Id = Guid.NewGuid(); 
+                  Name = ""; 
+                  Nickname = "apelido teste"; 
+                  BirthDay = DateTime.Now;
+                  Stack = [| "F#"; "Elixir"; "Clojure" |] }
+
+let InvalidPersonWithStackEmpty = {Id = Guid.NewGuid(); 
+                  Name = "Nome teste"; 
+                  Nickname = "apelido teste"; 
+                  BirthDay = DateTime.Now;
+                  Stack = [| "F#"; ""; "Clojure" |] }
+
+let ValidPersonWithStackNull = {Id = Guid.NewGuid(); 
+                  Name = "Nome teste"; 
+                  Nickname = "apelido teste"; 
+                  BirthDay = DateTime.Now;
+                  Stack = null }
+
+printfn "ValidPerson test is valid %b" (validPerson ValidPerson)
+printfn "InvalidPerson test is valid %b" (validPerson InvalidPerson)
+printfn "InvalidPersonWithStackEmpty test is valid %b" (validPerson InvalidPersonWithStackEmpty)
+printfn "ValidPersonWithStackNull test is valid %b" (validPerson ValidPersonWithStackNull)
